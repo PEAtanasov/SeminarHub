fab4f4112d4cdc8e5e2603fb24694fb049446396
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-02-18 15:02:53 +0200

    Common project created to hold validation constants

---
+++ Common/ValidationConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace Common
+{
+    public static class ValidationConstants
+    {
+        public static class SeminarConstants
+        {
+            public const int SeminarTopicMinLength = 3;
+            public const int SeminarTopicMaxLength = 100;
+            public const int SeminarLecturerMinLength = 5;
+            public const int SeminarLecturerMaxLength = 60;
+            public const int SeminarDetailsMinLength = 10;
+            public const int SeminarDetailsMaxLength = 500;
+            public const int SeminarDurationMinValue = 30;
+            public const int SeminarDurationMaxValue = 30;
+        }
+    }
+}
