5fac7ba3b2f7a4ffc0b7dd2178fde7776450aed6
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-02-18 15:29:28 +0200

    Fluent-api configuried. Initial migration created and applied. Database created

--- SeminarHub/Data/SeminarHubDbContext.cs
+++ SeminarHub/Data/SeminarHubDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SeminarHub.Data.Models;
 
 namespace SeminarHub.Data
 {
@@ -10,30 +11,42 @@ namespace SeminarHub.Data
         {
         }
 
+        public DbSet<Seminar> Seminars { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<SeminarParticipant> SeminarsParticipants { get; set; } = null!;
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            //builder
-            //   .Entity<Category>()
-            //   .HasData(new Category()
-            //   {
-            //       Id = 1,
-            //       Name = "Technology & Innovation"
-            //   },
-            //   new Category()
-            //   {
-            //       Id = 2,
-            //       Name = "Business & Entrepreneurship"
-            //   },
-            //   new Category()
-            //   {
-            //       Id = 3,
-            //       Name = "Science & Research"
-            //   },
-            //   new Category()
-            //   {
-            //       Id = 4,
-            //       Name = "Arts & Culture"
-            //   });
+            builder.Entity<SeminarParticipant>()
+            .HasKey(e => new { e.SeminarId, e.ParticipantId });
+
+            builder.Entity<SeminarParticipant>()
+            .HasOne(e => e.Seminar)
+            .WithMany(e => e.SeminarsParticipants)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+               .Entity<Category>()
+               .HasData(new Category()
+               {
+                   Id = 1,
+                   Name = "Technology & Innovation"
+               },
+               new Category()
+               {
+                   Id = 2,
+                   Name = "Business & Entrepreneurship"
+               },
+               new Category()
+               {
+                   Id = 3,
+                   Name = "Science & Research"
+               },
+               new Category()
+               {
+                   Id = 4,
+                   Name = "Arts & Culture"
+               });
 
             base.OnModelCreating(builder);
         }
