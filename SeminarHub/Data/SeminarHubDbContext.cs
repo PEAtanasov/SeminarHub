aea544c327f9a94e388ec0c84c5ef13198818c66
Petar Atanasov <123156553+PEAtanasov@users.noreply.github.com>
2024-02-18 19:38:44 +0200

    Done version 1

--- SeminarHub/Data/SeminarHubDbContext.cs
+++ SeminarHub/Data/SeminarHubDbContext.cs
@@ -23,7 +23,7 @@ namespace SeminarHub.Data
             builder.Entity<SeminarParticipant>()
             .HasOne(e => e.Seminar)
             .WithMany(e => e.SeminarsParticipants)
-            .OnDelete(DeleteBehavior.Restrict);
+            .OnDelete(DeleteBehavior.NoAction);
 
             builder
                .Entity<Category>()
